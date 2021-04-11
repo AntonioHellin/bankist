d5a811e31a96cfe736da69b4766ada3e6035b11a
Antonio Hellin <antonio.hellinh@gmail.com>
2021-04-11 19:41:13 +0200

    Working with Arrays. Building Bankist application

---
+++ lectures.js
@@ -0,0 +1,93 @@
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// // SLICE
+// // slice() method we can extract part of any array, but without changing the original array
+// console.log(arr.slice(2)); // Array with c, d and e
+// console.log(arr.slice(2, 4)); // Array starting from c and EXCLUDING e
+// console.log(arr.slice(-1)); // Array starting from last position
+// console.log(arr.slice(-2)); // Array starting from last position and showing the next one
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice()); // To create a shadow array
+// console.log([...arr]); // To create a shadow array
+
+// // SPLICE
+// // splice() method we can extract part of any array changing the original array
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(1, 2);
+// console.log(arr);
+
+// // REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2); // Reverse mutate also the original string
+
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// // JOIN
+// console.log(letters.join(' - '));
+
+// LESSON 142
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const [i, movement] of movements.entries()) {
+//   // for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// console.log('---- FOREACH ----');
+
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+/* 
+0: function(200)
+1: function(450)
+2: function(400)
+.
+.
+.
+*/
+
+// LESSON 143
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// // Map
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`);
+// });
