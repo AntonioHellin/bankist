8269deb096870472a482394a9937b094101ca562
Antonio Hellin <antonio.hellinh@gmail.com>
2021-04-17 10:06:52 +0200

    Summary: Which Array method to use?

--- lectures.js
+++ lectures.js
@@ -425,3 +425,80 @@ Differences between find() and filter():
 
 // const z = Array.from({ length: 7 }, (_, i) => i + 1);
 // console.log(z);
+
+// LESSON 163: WHICH ARRAY AMETHOD TO USE?
+
+/*
+What i want to actually want from the method?
+
+1. To mutate original array
+
+- Add to original: 
+.push (end)
+.unshift (start)
+
+- Remove from original_
+.pop (end)
+.shift (start)
+.splice (any)
+
+- Others
+.reverse
+.sort
+.fill
+
+2. A new array
+
+- Computed from original:
+.map
+
+-Filtered using condition:
+.filter
+
+- Portion of original:
+.slice
+
+- Adding original to other:
+.concat
+
+- Flattening the original:
+.flat
+.flatMap
+
+3. An array index
+
+- Based on value:
+.indexOf
+
+- Based on test condition:
+.findIndex
+
+4. Know if array includes
+
+- Based on value:
+.includes
+
+- Based on test condition:
+.some
+.every
+
+5. To transform to value
+
+- Based on accumulator:
+.reduce (Boil down array to single value of any type: number, string, boolean, or even new array or object)
+
+6. An array element
+
+- Based on test condition:
+.find
+
+7. A new string
+
+- Based on separator string:
+.join
+
+8. To just loop array
+
+- Based on callback:
+.forEach (Does not create a new array, just loops over it)
+*/
