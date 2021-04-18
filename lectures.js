70d78d2eb3774f8f8dea84ed1d05486a05b7614b
Antonio Hellin <antonio.hellinh@gmail.com>
2021-04-18 11:53:01 +0200

    Comment the code of the exercises

--- lectures.js
+++ lectures.js
@@ -154,7 +154,7 @@ FILTER:  to filter element in the original array which satisy a certain conditio
 REDUCE: reduce boils (reduces) all array elements down to one single value (example: adding all elements together). Returns the reduce value.
 */
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // const eurToUsd = 1.1;
 
@@ -612,12 +612,12 @@ recommended portion.
 
 */
 
-const dogs = [
-  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
-  { weight: 8, curFood: 200, owners: ['Matilda'] },
-  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
-  { weight: 32, curFood: 340, owners: ['Michael'] },
-];
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+// ];
 
 // movements.forEach(function (mov, i, arr) {
 //   if (mov > 0) {
@@ -628,49 +628,49 @@ const dogs = [
 // });
 
 // 1.
-dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
-console.log(dogs);
+// dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+// console.log(dogs);
 
-// 2.
-const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
-console.log(dogSarah);
-console.log(
-  `Sarah's dog is eating too ${
-    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
-  }`
-);
+// // 2.
+// const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+// console.log(dogSarah);
+// console.log(
+//   `Sarah's dog is eating too ${
+//     dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+//   }`
+// );
 
-// 3.
-const ownersEatTooMuch = dogs
-  .filter(dog => dog.curFood > dog.recFood)
-  .flatMap(dog => dog.owners);
-console.log(ownersEatTooMuch);
-const ownersEatTooLittle = dogs
-  .filter(dog => dog.curFood < dog.recFood)
-  .flatMap(dog => dog.owners);
-console.log(ownersEatTooLittle);
+// // 3.
+// const ownersEatTooMuch = dogs
+//   .filter(dog => dog.curFood > dog.recFood)
+//   .flatMap(dog => dog.owners);
+// console.log(ownersEatTooMuch);
+// const ownersEatTooLittle = dogs
+//   .filter(dog => dog.curFood < dog.recFood)
+//   .flatMap(dog => dog.owners);
+// console.log(ownersEatTooLittle);
 
-// 4.
-// Log a string to the console for each array created in 3., like this: "Matilda and
-// Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat
-// too little!"
-console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
-console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+// // 4.
+// // Log a string to the console for each array created in 3., like this: "Matilda and
+// // Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat
+// // too little!"
+// console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+// console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
 
-// 5.
-console.log(dogs.some(dog => dog.curFood === dog.recFood));
+// // 5.
+// console.log(dogs.some(dog => dog.curFood === dog.recFood));
 
-// 6.
-// current > (recommended * 0.90) && current < (recommended * 1.10)
-const checkEatingOk = dog =>
-  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+// // 6.
+// // current > (recommended * 0.90) && current < (recommended * 1.10)
+// const checkEatingOk = dog =>
+//   dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
 
-console.log(dogs.some(checkEatingOk));
+// console.log(dogs.some(checkEatingOk));
 
-// 7.
-console.log(dogs.filter(checkEatingOk));
+// // 7.
+// console.log(dogs.filter(checkEatingOk));
 
-// 8.
+// // 8.
 
-const dogsCopy = dogs.slice().sort((a, b) => a.recFood - b.recFood);
-console.log(dogsCopy);
+// const dogsCopy = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+// console.log(dogsCopy);
